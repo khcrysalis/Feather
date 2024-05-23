8c655da18be592707d1cb4db476439d47a4b396d
Samara <97859147+khcrysalis@users.noreply.github.com>
2024-05-22 21:05:38 -0700

    dumb shit

--- feather/Delegates/AppDelegate.swift
+++ feather/Delegates/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Nuke
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		UserDefaults.standard.set(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, forKey: "currentVersion")
-
+		
+		imagePipline()
+		
 		let tabBarController = TabbarController()
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.rootViewController = tabBarController
@@ -40,5 +43,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			}
 		}
 	}
+	
+	func imagePipline() {
+		DataLoader.sharedUrlCache.diskCapacity = 0
+		let pipeline = ImagePipeline {
+			let dataLoader: DataLoader = {
+				let config = URLSessionConfiguration.default
+				config.urlCache = nil
+				return DataLoader(configuration: config)
+			}()
+			let dataCache = try? DataCache(name: "com.ssalggnikool.pointercrate.datacache") // disk cache
+			let imageCache = Nuke.ImageCache() // memory cache
+			dataCache?.sizeLimit = 500 * 1024 * 1024
+			imageCache.costLimit = 100 * 1024 * 1024
+			$0.dataCache = dataCache
+			$0.imageCache = imageCache
+			$0.dataLoader = dataLoader
+			$0.dataCachePolicy = .automatic
+			$0.isStoringPreviewsInMemoryCache = false
+		}
+		ImagePipeline.shared = pipeline
+	}
+	
 }
 
