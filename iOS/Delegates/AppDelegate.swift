b3ca7e8cab30b555dbd3ca7732c2345fea986266
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2024-08-19 10:48:34 -0700

    chore: don't pipe at all

--- iOS/Delegates/AppDelegate.swift
+++ iOS/Delegates/AppDelegate.swift
@@ -14,21 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	static let isSideloaded = Bundle.main.bundleIdentifier != "kh.crysalis.feather"
 	var window: UIWindow?
-	#if !targetEnvironment(simulator)
-	var savedStdout: Int32 = -1
-	var savedStderr: Int32 = -1
-	var outPipe: Pipe?
-	var fileHandle: FileHandle?
-	var semaphore: DispatchSemaphore?
-	#endif
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		UserDefaults.standard.set(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, forKey: "currentVersion")
 		addDefaultRepos()
 		imagePipline()
-		#if !targetEnvironment(simulator)
-		startPiping()
-		#endif
+
 		let tabBarController = TabbarController()
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.rootViewController = tabBarController
@@ -112,51 +103,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		ImagePipeline.shared = pipeline
 	}
 }
-#if !targetEnvironment(simulator)
-extension AppDelegate {
-	func startPiping() {
-		outPipe = Pipe()
-		semaphore = DispatchSemaphore(value: 0)
-		
-		
-		let fileManager = FileManager.default
-		let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-		let logFileURL = documentsDirectory.appendingPathComponent("stdout.log")
-		
-		if fileManager.fileExists(atPath: logFileURL.path) {
-			do { try! fileManager.removeItem(at: logFileURL) }
-		}
-		
-		FileManager.default.createFile(atPath: logFileURL.path, contents: nil, attributes: nil)
-		fileHandle = try? FileHandle(forWritingTo: logFileURL)
-		savedStdout = dup(STDOUT_FILENO)
-		savedStderr = dup(STDERR_FILENO)
-		dup2(outPipe!.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
-		dup2(outPipe!.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
-		
-		outPipe!.fileHandleForReading.readabilityHandler = { [weak self] fileHandle in
-			let data = fileHandle.availableData
-			if data.isEmpty {
-				fileHandle.readabilityHandler = nil
-				self?.semaphore?.signal()
-			} else {
-				_ = String(data: data, encoding: .utf8) ?? ""
-				self?.fileHandle?.write(data)
-			}
-		}
-		
-		setvbuf(stdout, nil, _IONBF, 0)
-		setvbuf(stderr, nil, _IONBF, 0)
-	}
-	func applicationWillTerminate(_ application: UIApplication) {
-		dup2(savedStdout, STDOUT_FILENO)
-		dup2(savedStderr, STDERR_FILENO)
-		close(savedStdout)
-		close(savedStderr)
-		
-		fileHandle?.closeFile()
-		outPipe?.fileHandleForWriting.closeFile()
-		semaphore?.wait()
-	}
-}
-#endif
+
