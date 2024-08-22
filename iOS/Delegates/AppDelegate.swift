136536cc75bb06bd38f7cb2055e888c935bc09eb
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2024-08-22 01:24:35 +0100

    Update AppDelegate.swift

--- iOS/Delegates/AppDelegate.swift
+++ iOS/Delegates/AppDelegate.swift
@@ -117,56 +117,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIOnboardingViewControlle
 	}
 }
 
-#if !targetEnvironment(simulator)
-extension AppDelegate {
-	func startPiping() {
-		outPipe = Pipe()
-		semaphore = DispatchSemaphore(value: 0)
-		
-		
-		let fileManager = FileManager.default
-		let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-		let logFileURL = documentsDirectory.appendingPathComponent("stdout.log")
-		
-		if fileManager.fileExists(atPath: logFileURL.path) {
-			do { try! fileManager.removeItem(at: logFileURL) }
-		}
-		
-		FileManager.default.createFile(atPath: logFileURL.path, contents: nil, attributes: nil)
-		fileHandle = try? FileHandle(forWritingTo: logFileURL)
-		savedStdout = dup(STDOUT_FILENO)
-		savedStderr = dup(STDERR_FILENO)
-		dup2(outPipe!.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
-		dup2(outPipe!.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
-		
-		outPipe!.fileHandleForReading.readabilityHandler = { [weak self] fileHandle in
-			let data = fileHandle.availableData
-			if data.isEmpty {
-				fileHandle.readabilityHandler = nil
-				self?.semaphore?.signal()
-			} else {
-				_ = String(data: data, encoding: .utf8) ?? ""
-				self?.fileHandle?.write(data)
-			}
-		}
-		
-		setvbuf(stdout, nil, _IONBF, 0)
-		setvbuf(stderr, nil, _IONBF, 0)
-	}
-	func applicationWillTerminate(_ application: UIApplication) {
-		dup2(savedStdout, STDOUT_FILENO)
-		dup2(savedStderr, STDERR_FILENO)
-		close(savedStdout)
-		close(savedStderr)
-		
-		fileHandle?.closeFile()
-		outPipe?.fileHandleForWriting.closeFile()
-		semaphore?.wait()
-	}
-}
-#endif
-
-
 extension UIOnboardingViewConfiguration {
   static func setUp() -> Self {
     let welcomeToLine = NSMutableAttributedString(string: "Welcome to")
