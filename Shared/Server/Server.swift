a21df12991d96b4122795b350541fa138188e4e1
久违. <112636290+jiuwei95@users.noreply.github.com>
2025-04-17 01:24:00 +0800

    fix: URL encoding for non-ASCII characters in online installation (#319)

--- Shared/Server/Server.swift
+++ Shared/Server/Server.swift
@@ -82,8 +82,10 @@ class Installer: Identifiable, ObservableObject {
 		}
 		
 		app.get("i") { req -> Response in
-
-			let testurl = "itms-services://?action=download-manifest&url=" + ("\(Preferences.onlinePath ?? Preferences.defaultInstallPath)/genPlist?bundleid=\(metadata.id)&name=\(metadata.name)&version=\(metadata.name)&fetchurl=\(self.payloadEndpoint.absoluteString)").addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+			let baseUrl = "\(Preferences.onlinePath ?? Preferences.defaultInstallPath)/genPlist?bundleid=\(metadata.id)&name=\(metadata.name)&version=\(metadata.name)&fetchurl=\(self.payloadEndpoint.absoluteString)"
+			let encodedBaseUrl = baseUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+			let finalEncodedUrl = encodedBaseUrl.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+			let testurl = "itms-services://?action=download-manifest&url=" + finalEncodedUrl
 
 			var html = ""
 			
