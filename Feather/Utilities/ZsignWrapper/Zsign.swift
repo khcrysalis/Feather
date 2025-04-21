09b7f20420756912fe4388889c2e7ea993554887
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2025-04-21 02:27:06 -0700

    chore: docs for zsign

--- Feather/Utilities/ZsignWrapper/Zsign.swift
+++ Feather/Utilities/ZsignWrapper/Zsign.swift
@@ -8,22 +8,51 @@
 import Zsign
 
 final class Zsign {
+	/// Injects a load command to an executable
+	/// - Parameters:
+	///   - appExecutable: Executable
+	///   - path: Load command (i.e. `@rpath/CydiaSubstrate.framework`)
+	///   - weak: Weak inject
+	/// - Returns: True if its successful
 	static func injectDyLib(appExecutable: String, with path: String, weak: Bool = true) -> Bool {
 		InjectDyLib(appExecutable, path, weak)
 	}
-	
+	/// Removes load commands from an executable
+	/// - Parameters:
+	///   - appExecutable: Executable
+	///   - dylibs: Load commands (i.e. `@rpath/CydiaSubstrate.framework...`)
+	/// - Returns: True if its successful
 	static func removeDylibs(appExecutable: String, using dylibs: [String]) -> Bool  {
 		UninstallDylibs(appExecutable, dylibs)
 	}
-	
+	/// List load commands from an executable
+	/// - Parameter appExecutable: Executable
+	/// - Returns: String array with load commands if any
 	static func listDylibs(appExecutable: String) -> [String] {
 		ListDylibs(appExecutable)
 	}
-	
+	/// Matches and replaces load commands to an executable
+	/// - Parameters:
+	///   - appExecutable: Executable
+	///   - old: Old load command (i.e. `/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate`)
+	///   - new: New load command (i.e. `@rpath/CydiaSubstrate.framework/CydiaSubstrate`)
+	/// - Returns: True if its successful
 	static func changeDylibPath(appExecutable: String, for old: String, with new: String) -> Bool {
 		ChangeDylibPath(appExecutable, old, new)
 	}
-	
+	/// Signs a folder (application bundle) using Zsign
+	/// - Parameters:
+	///   - appPath: Relative path to app bundle
+	///   - provisionPath: Relative path to a provisioning file (i.e. `samara.mobileprovision`)
+	///   - p12Path: Relative path to a key file (i.e. `samara.p12`)
+	///   - p12Password: Password to the key file
+	///   - entitlementsPath: Relative path to an entitlements file
+	///   - customIdentifier: Custom indentifier for the app bundle
+	///   - customName: Custom display name for the app bundle
+	///   - customVersion: Custom version for the app bundle
+	///   - adhoc: If the app bundle should be signed using Adhoc (no signing identity)
+	///   - removeProvision: If `embedded.mobileprovision` should be excluded when signing
+	/// - Returns: True if its successful
 	static func sign(
 		appPath: String = "",
 		provisionPath: String = "",
