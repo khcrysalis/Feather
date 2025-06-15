e53c4c0be9573b04552ddaecda2d1342c22f5abc
👒 <sam4r16@gmail.com>
2025-06-14 21:53:32 -0700

    Feather(Views): Prompt the user to star repository if needed

--- Feather/Views/Sources/SourcesView.swift
+++ Feather/Views/Sources/SourcesView.swift
@@ -14,6 +14,8 @@ import NimbleViews
 struct SourcesView: View {
 	@Environment(\.horizontalSizeClass) private var horizontalSizeClass
 	
+	@AppStorage("Feather.shouldStar") private var _shouldStar: Int = 0
+	
 	@StateObject var viewModel = SourcesViewModel.shared
 	@State private var _isAddingPresenting = false
 	@State private var _addingSourceLoading = false
@@ -91,6 +93,22 @@ struct SourcesView: View {
 		}
 		.task(id: Array(_sources)) {
 			await viewModel.fetchSources(_sources)
+		}
+		.onAppear {
+			guard _shouldStar < 6 else { return }; _shouldStar += 1
+			guard _shouldStar == 6 else { return }
+			
+			let github = UIAlertAction(title: "GitHub", style: .default) { _ in
+				UIApplication.open("https://github.com/khcrysalis/Feather")
+			}
+			
+			let cancel = UIAlertAction(title: .localized("Dismiss"), style: .cancel)
+			
+			UIAlertController.showAlert(
+				title: .localized("Enjoying %@?", arguments: Bundle.main.name),
+				message: .localized("Go to our GitHub and give us a star!"),
+				actions: [github, cancel]
+			)
 		}
 	}
 }
