8a73cdeb6f29410bceee1e0e406e49562422b219
khcrysalis <sam4r16@gmail.com>
2025-11-25 08:14:26 -0800

    Revert "fix: remove dispatch and add localized strings"
    This reverts commit 91b2eb4be7e086282c57199e351f0a85b59c7471.

--- Feather/Views/Sources/Apps/SourceAppsView.swift
+++ Feather/Views/Sources/Apps/SourceAppsView.swift
@@ -94,19 +94,23 @@ struct SourceAppsView: View {
 			
 			Button(.localized("Copy"), systemImage: "doc.on.doc") {
 				guard !object.isEmpty else {
-					UIAlertController.showAlertWithOk(
-						title: .localized("Error"),
-						message: .localized("No sources to copy")
-					)
+					DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+						UIAlertController.showAlertWithOk(
+							title: .localized("Error"),
+							message: .localized("No sources to copy")
+						)
+					}
 					return
 				}
 				UIPasteboard.general.string = object.map {
 					$0.sourceURL!.absoluteString
 				}.joined(separator: "\n")
-				UIAlertController.showAlertWithOk(
-					title: .localized("Success"),
-					message: .localized("Sources copied to clipboard")
-				)
+				DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+					UIAlertController.showAlertWithOk(
+						title: .localized("Success"),
+						message: .localized("Sources copied to clipboard")
+					)
+				}
 			}
 		}
 		.toolbar {
