5d7326b1e255935506ed20bfefe00b62c0e62cc7
khcrysalis <sam4r16@gmail.com>
2026-01-13 00:36:29 -0800

    feat: experiment for IconServices

---
+++ Feather/Utilities/AppIcon.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FRIconServicesRenderer : NSObject
++ (nullable UIImage *)iconForBundle:(NSBundle *)bundle;
+@end
+
+NS_ASSUME_NONNULL_END
--- Feather/Views/Common/FRAppIconView.swift
+++ Feather/Views/Common/FRAppIconView.swift
@@ -11,21 +11,60 @@ struct FRAppIconView: View {
 	private var _app: AppInfoPresentable
 	private var _size: CGFloat
 	
+	@State private var loadedIcon: UIImage? = nil
+	@State private var isLoading = true
+	
 	init(app: AppInfoPresentable, size: CGFloat = 87) {
 		self._app = app
 		self._size = size
 	}
 	
 	var body: some View {
-		if
-			let iconFilePath = Storage.shared.getAppDirectory(for: _app)?.appendingPathComponent(_app.icon ?? ""),
-			let uiImage = UIImage(contentsOfFile: iconFilePath.path)
-		{
-			Image(uiImage: uiImage)
-				.appIconStyle(size: _size)
-		} else {
-			Image("App_Unknown")
-				.appIconStyle(size: _size)
+		Group {
+			if let uiImage = loadedIcon {
+				Image(uiImage: uiImage)
+					.appIconStyle(size: _size)
+			} else if isLoading {
+				Image("App_Unknown")
+					.appIconStyle(size: _size)
+			} else {
+				Image("App_Unknown")
+					.appIconStyle(size: _size)
+			}
 		}
+		.task(id: _app.uuid) {
+			await loadIconAsync()
+		}
+	}
+	
+	private func loadIconAsync() async {
+		let image = await Task.detached(priority: .userInitiated) {
+			return await self.resolveIcon()
+		}.value
+		
+		await MainActor.run {
+			self.loadedIcon = image
+			self.isLoading = false
+		}
+	}
+
+	private func resolveIcon() -> UIImage? {
+		guard let bundleURL = Storage.shared.getAppDirectory(for: _app) else {
+			return nil
+		}
+		
+		// 1. System Render (Heavy)
+		if let bundle = Bundle(url: bundleURL),
+		   let systemIcon = FRIconServicesRenderer.icon(for: bundle) {
+			return systemIcon
+		}
+
+		// 2. Manual Fallback
+		if let iconName = _app.icon {
+			let iconPath = bundleURL.appendingPathComponent(iconName).path
+			return UIImage(contentsOfFile: iconPath)
+		}
+		
+		return nil
 	}
 }
