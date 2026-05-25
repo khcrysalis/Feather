b17f573cfcab8c19d41602b51560b40eb20dc71a
Ismail Carlik <ismailcarlik@github.com>
2026-05-25 10:22:30 +0300

    Feature: Search in app list by bundle identifier.

--- Feather/Views/Sources/Apps/UIKit/SourceAppsTableRepresentableView.swift
+++ Feather/Views/Sources/Apps/UIKit/SourceAppsTableRepresentableView.swift
@@ -134,6 +134,7 @@ extension SourceAppsTableRepresentableView { class Coordinator: NSObject, UITabl
 	private func _calculateSortedApps() -> [(source: ASRepository, app: ASRepository.App)] {
 		let filtered = _allAppsWithSource.filter {
 			searchText.isEmpty ||
+				($0.app.id?.localizedCaseInsensitiveContains(searchText) ?? false) ||
 				($0.app.name?.localizedCaseInsensitiveContains(searchText) ?? false) ||
 				($0.app.description?.localizedCaseInsensitiveContains(searchText) ?? false) ||
 				($0.app.subtitle?.localizedCaseInsensitiveContains(searchText) ?? false) ||
